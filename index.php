e1eb409ef79d45f01cc0ed5401124d4a63cdebb3
MaZhirong <fingnet@gmail.com>
2013-10-07 12:02:46 +0800

    init

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
 <?php
-<<<<<<< HEAD
-/**
- * Created by JetBrains PhpStorm.
- * User: lsymac
- * Date: 13-8-9
- * Time: 下午4:22
- * To change this template use File | Settings | File Templates.
- */
- 
-=======
-  echo 'Welcome to Glosea';
-?>
->>>>>>> 47eb012b88241705160f2110c15eaa0bb49c8e86
+
