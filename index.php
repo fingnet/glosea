60b9c159f7f10ee951c5b479caa7e6aba98361b0
lanshayu <fingnet@gmail.com>
2014-03-23 14:24:34 +0800

    初始化入口

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
-/**
- * 系统路由
- * @author MaZhirong <fingnet@gmail.com> 2014-3-14
- * @copyright ©2103 glosea.com
- * @license http://www.glosea.com
- * @version 0.1
- */
- 
- ?>
+include 'glosea/runtime';
