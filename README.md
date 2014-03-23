aaca43683cd3524639a947ef8194883a7c7eaa91
lanshayu <fingnet@gmail.com>
2014-03-23 22:13:46 +0800

    更正应用配置文件名称

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Glosea是基于PHP5.3+Mysql的Web应用平台。
 - 基于PHP5.3；
 - 可以使用匿名函数；
 - 魔术函数_\_invoke(), _\_callStatic()；
+- 使用命名空间；
 - spl_autoload_register，类自动加载；
 - PDO作为关系型数据库适配器；
 - 使用const定义常量；
---
+++ data/config/apps.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+	'apps' => array(
+		'glosea.site' => array(
+			'app_id'   => 'glosea.site',
+			'app_name' => '网站管理'
+		),
+		'glosea.site.admin' => array(
+			'app_id'   => 'glosea.site',
+			'app_name' => '网站管理系统'
+		),
+		'glosea.site.api' => array(
+			'app_id'   => 'glosea.site.api',
+			'app_name' => '接口引擎'
+		),
+		'xiyouqi.order' => array(
+			'app_id'   => 'xiyouqi.order',
+			'app_name' => '订餐平台'
+		)
+	),
+	'default' => 'glosea.site'
+);
