a28bbcf9c5bf0ac064f2a0d4d9e67fbf144f6845
lanshayu <fingnet@gmail.com>
2014-04-04 22:05:52 +0800

    缩进更新

--- glosea/framework/base/AbstractModel.php
+++ glosea/framework/base/AbstractModel.php
@@ -66,29 +66,29 @@ abstract class AbstractModel implements ArrayAccess,IteratorAggregate,Countable 
 	
 	//实现数组迭代器接口
 	public function getIterator() {
-        return new ArrayIterator($this -> attrs);
-    }
+		return new ArrayIterator($this -> attrs);
+	}
 	
 	//实现数组数据访问接口
 	public function offsetGet($offset) {
-        return isset($this -> attrs[$offset]) ? $this -> attrs[$offset] : null;
-    }
+		return isset($this -> attrs[$offset]) ? $this -> attrs[$offset] : null;
+	}
 	
-    public function offsetExists($offset) {
-        return isset($this -> attrs[$offset]);
-    }
+	public function offsetExists($offset) {
+		return isset($this -> attrs[$offset]);
+	}
 	
 	public function offsetSet($offset, $value) {
-        if (is_null($offset)) {
-            $this -> attrs[] = $value;
-        } else {
-            $this -> attrs[$offset] = $value;
-        }
-    }
+		if (is_null($offset)) {
+			$this -> attrs[] = $value;
+		} else {
+			$this -> attrs[$offset] = $value;
+		}
+	}
 	
-    public function offsetUnset($offset) {
-        unset($this -> attrs[$offset]);
-    }
+	public function offsetUnset($offset) {
+		unset($this -> attrs[$offset]);
+	}
 	
 	//实现数据统计接口
 	public function count() {
--- glosea/framework/base/Collection.php
+++ glosea/framework/base/Collection.php
@@ -13,32 +13,32 @@ class Collection implements ArrayAccess,IteratorAggregate,Countable {
 	}
 	
 	public function getIterator() {
-        return new ArrayIterator($this -> items);
-    }
+		return new ArrayIterator($this -> items);
+	}
 	
 	public function offsetGet($offset) {
-        return isset($this -> items[$offset]) ? $this -> items[$offset] : null;
-    }
+		return isset($this -> items[$offset]) ? $this -> items[$offset] : null;
+	}
 	
-    public function offsetExists($offset) {
-        return isset($this -> items[$offset]);
-    }
+	public function offsetExists($offset) {
+		return isset($this -> items[$offset]);
+	}
 	
 	public function offsetSet($offset, $value) {
-        if (is_null($offset)) {
-            $this -> items[] = $value;
-        } else {
-            $this -> items[$offset] = $value;
-        }
-    }
+		if (is_null($offset)) {
+			$this -> items[] = $value;
+		} else {
+			$this -> items[$offset] = $value;
+		}
+	}
 	
-    public function offsetUnset($offset) {
-        unset($this -> items[$offset]);
-    }
+	public function offsetUnset($offset) {
+		unset($this -> items[$offset]);
+	}
 	
 	public function count() {
-        return count($this -> items);
-    }
+		return count($this -> items);
+	}
 	
 	public function add(array $items, $merge = true){
 		if($merge){
